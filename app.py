d2bb5bd92a69399a68861a3c9486244edf2d2cdf
AlancitoPaladin <alancarlos032104@gmail.com>
2025-02-26 13:44:31 -0600

    Initial Changes

--- app.py
+++ app.py
@@ -8,5 +8,14 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/courses')
+def courses():
+    Courses = [
+        {"Course": "Ingeniería de Software", "Duration": "Feb-Junio 2025", "Teacher": "Alan Carlos Hernández",
+         "Image": "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*gQzkQ3uJ0SwJL51t16bivw.jpeg"}
+    ]
+    return Courses
+
+
 if __name__ == '__main__':
     app.run()
