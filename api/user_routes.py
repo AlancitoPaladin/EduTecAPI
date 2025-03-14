a79812d0a4901620cc0940d68c08badd733e4c0c
AlancitoPaladin <alancarlos032104@gmail.com>
2025-03-13 19:19:03 -0600

    New changes of today

--- api/user_routes.py
+++ api/user_routes.py
@@ -8,17 +8,24 @@ user_bp = Blueprint("user_bp", __name__)
 @user_bp.route('/courses', methods=["GET"])
 def courses():
     page = request.args.get("page", 1, type=int)
-    per_page = request.args.get("per_page", 10, type=int)
+    per_page = request.args.get("per_page", 20, type=int)
     courses_collection = mongo.db.courses
 
+    projection = {
+        "_id": 0,
+        "course": 1,
+        "image": 1,
+        "stars": 1,
+        "description": 1
+    }
+
     try:
-        courses_cursor = courses_collection.find() \
+        courses_cursor = courses_collection.find({}, projection) \
             .skip((page - 1) * per_page) \
             .limit(per_page)
         courses_list = []
 
         for course in courses_cursor:
-            course["_id"] = str(course["_id"])
             courses_list.append(course)
 
         return jsonify({
@@ -31,9 +38,20 @@ def courses():
         return jsonify({"message": "Ocurrió un error", "error": str(e)}), 500
 
 
+@user_bp.route('/course/<id>', methods=["GET"])
+def course(id):
+    course = mongo.db.courses.find_one({"_id": ObjectId(id)})
+    if not course:
+        return jsonify({"message": "Course not found"}), 404
+    else:
+        course["_id"] = str(course["_id"])
+        return jsonify(course), 200
+
+
 @user_bp.route('/insert_course', methods=["GET"])
 def insert_course():
     pass
+
 
 @user_bp.route('/update_info', methods=["POST"])
 def update_info():
